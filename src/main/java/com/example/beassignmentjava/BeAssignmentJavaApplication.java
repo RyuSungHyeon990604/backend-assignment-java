846fb54523cb363c597944683fdb999d7732b908
RyuSungHyeon990604 <shryoo990604@gmail.com>
2025-06-09 13:54:37 +0900

    init project

---
+++ src/main/java/com/example/beassignmentjava/BeAssignmentJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.beassignmentjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeAssignmentJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeAssignmentJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/beassignmentjava/BeAssignmentJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.beassignmentjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeAssignmentJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
