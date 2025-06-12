cba9fe3dd1c3702c95c9ade3399c104327b65043
RyuSungHyeon990604 <shryoo990604@gmail.com>
2025-06-12 19:47:34 +0900

    🐛 Fix : swagger 현재 도메인 기준으로 api호출하도록 수정

--- src/main/java/com/example/beassignmentjava/config/swagger/SwaggerConfig.java
+++ src/main/java/com/example/beassignmentjava/config/swagger/SwaggerConfig.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -18,6 +19,7 @@ public class SwaggerConfig {
 				.title("BackEnd-Assignment-Java API")
 				.description("바로인턴 직무과제 (JAVA)")
 			).components(jwtComponents())
+			.addServersItem(new Server().url("/"))
 			.addSecurityItem(new SecurityRequirement().addList("jwt"));
 	}
 
